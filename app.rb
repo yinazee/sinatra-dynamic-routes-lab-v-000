828a3f2f24109b5c5457e55fb227ca9b2779a419
Yina Zhu <yinagit@gmail.com>
2018-05-22 02:39:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,11 +26,11 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    # num1 = params[:number1].to_i
-    # num2 = params[:number2].to_i
-    # binding.pry
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    binding.pry
 
-    # params[:operation] == * || + || - || /
+
   end
 
 end
