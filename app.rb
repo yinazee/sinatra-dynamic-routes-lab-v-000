e202cac9c35bdde26b2c75fcfc4ef1255a7d50cd
Yina Zhu <yinagit@gmail.com>
2018-05-22 02:15:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
 
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    params[[:word1],[:word2],[:word3],[:word4],[:word5]].join(" ")
+    @5_word_phrase = params[[:word1],[:word2],[:word3],[:word4],[:word5]].join(" ")
     binding.pry
   end
 
